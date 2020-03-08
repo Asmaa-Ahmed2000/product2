c8647a5115bf63a30b4e87301e53cc57e0b3b12c
Asmaa-Ahmed2000 <51019738+Asmaa-Ahmed2000@users.noreply.github.com>
2020-03-08 03:18:12 +0200

    product2

---
+++ assets/js/scripts.js
@@ -0,0 +1,19 @@
+// Add Your Scripts here
+
+// OWL-CAROUSEL
+$('.review-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    rtl: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 1000,
+    responsive:{
+        0:{
+            items:1,
+        }
+    }
+})
+// OWL-CAROUSEL-END
